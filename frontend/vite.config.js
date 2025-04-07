c5fb814ac0694bfb90bb12cacd0420e00b6bbad2
KOUSHIKG04 <koushik.20201ise0024@presidencyuniversity.in>
2025-04-07 11:44:47 +0530

    updated gitignore

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -20,4 +20,16 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+    minify: "terser",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+  },
 });
